cf0c6530dc6d28af51500121a9e7801de8cb1f4e
Yury Antonov <1390348+yantonov@users.noreply.github.com>
2024-06-14 00:15:31 +0200

    remove build dir from vcs, create it when needed automatically

--- bin/build-demo-server.sh
+++ bin/build-demo-server.sh
@@ -3,4 +3,6 @@ set -o errexit -o nounset
 
 cd "$(dirname "$0")/.."
 
+mkdir -p target
+
 go build -o target/jira-demo-server cmd/jira-demo-server/main.go
--- bin/build.sh
+++ bin/build.sh
@@ -3,5 +3,7 @@ set -o errexit -o nounset
 
 cd "$(dirname "$0")/.."
 
+mkdir -p target
+
 go build -o target/jira-issue-selector cmd/jira-issue-selector/main.go 
 
