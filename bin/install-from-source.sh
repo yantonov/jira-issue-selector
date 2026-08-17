836cdb72bacf681272eeefb4eed8cfac1af4046e
Yury Antonov <1390348+yantonov@users.noreply.github.com>
2026-08-17 21:49:19 +0200

    use ~/.local/bin and not ~/bin

--- bin/install-from-source.sh
+++ bin/install-from-source.sh
@@ -4,12 +4,14 @@ set -o errexit -o nounset
 cd "$(dirname "$0")/.."
 
 INSTALL_DIR="${HOME}/bin"
+
 mkdir -p "${INSTALL_DIR}"
 
 BUILD_ARTIFACT="target/jira-issue-selector"
 
 if [ -f "${BUILD_ARTIFACT}" ]; then
     cp target/jira-issue-selector "${INSTALL_DIR}"
+    echo "Installed to ${INSTALL_DIR}"
 else
     echo "binary artifact not found"
     echo "build it first: ./bin/build.sh"
